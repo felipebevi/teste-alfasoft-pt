9147b3b42d47f3d534b19076d9154640c1ba1b4f
Felipe Bevi <felipe@9ideias.com>
2026-02-09 10:28:05 -0300

    view de visualizacao de detalhes do contato, simples, OK

---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', $contact->name)
+
+@section('content')
+    <h1>Contact Details</h1>
+
+    <table class="table">
+        <tr>
+            <th width="150">ID</th>
+            <td>{{ $contact->id }}</td>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <td>{{ $contact->name }}</td>
+        </tr>
+        <tr>
+            <th>Contact</th>
+            <td>{{ $contact->contact }}</td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <td>{{ $contact->email }}</td>
+        </tr>
+    </table>
+
+    <a href="{{ route('contacts.edit', $contact) }}" class="btn btn-warning">Edit</a>
+    <form action="{{ route('contacts.destroy', $contact) }}" method="POST" class="d-inline">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+    </form>
+    <a href="{{ route('contacts.index') }}" class="btn btn-secondary">Back to List</a>
+@endsection
